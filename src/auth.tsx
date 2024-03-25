1b43cfed865986940c1c2e48397973f9f786771a
Daniel Godin <DanielEGodin@gmail.com>
2024-03-25 11:20:53 -0700

    Moved auth functions into auth.tsx, away from firebase.tsx

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,13 +1,10 @@
 import { auth } from "./firebase"
 
-
-
-
-
-
-
-
-
+import { 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+} from 'firebase/auth'
 
 
 export function AuthForm() {
@@ -34,3 +31,29 @@ export function AuthForm() {
 
     )
 }
+
+// if (window.location.hostname === 'localhost') {
+//     connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+// }
+
+// createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//         // Signed Up
+//         const user = userCredential.user;
+//         // ...
+//     })
+//     .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+// });
+
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+// });
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,46 +1,7 @@
 import { app } from "../firebase-config";
-import { 
-    getAuth,
-    connectAuthEmulator,
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-} from "firebase/auth";
-import { 
-    getFirestore,
-    connectFirestoreEmulator,
-} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Firebase Initializations:
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Firebase Functions:
-
-// if (window.location.hostname === 'localhost') {
-//     connectAuthEmulator(auth, 'http://127.0.0.1:9099');
-// }
-
-// createUserWithEmailAndPassword(auth, email, password)
-//     .then((userCredential) => {
-//         // Signed Up
-//         const user = userCredential.user;
-//         // ...
-//     })
-//     .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-// });
-
-// signInWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in 
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-// });
-
-
